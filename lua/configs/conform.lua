695e473f5e6d422c7f59d32fb689eaa19cc3b88a
Sudip Chhantyal <oryn2gs@gmail.com>
2025-03-30 14:30:29 +0545

    FIXES: fixed conform formatter syntax.

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -3,19 +3,19 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
-    javascript = { { "prettierd", "prettier" } },
-    javascriptreact = { { "prettierd", "prettier" } },
-    typescript = { { "prettierd", "prettier" } },
-    typescriptreact = { { "prettierd", "prettier" } },
+    javascript = { "prettierd", "prettier" },
+    javascriptreact = { "prettierd", "prettier" },
+    typescript = { "prettierd", "prettier" },
+    typescriptreact = { "prettierd", "prettier" },
     python = { "isort", "black" },
   },
-
   format_on_save = {
     -- These options will be passed to conform.format()
     lsp_fallback = true,
     async = false,
     timeout_ms = 2000,
   },
+  stop_after_first = true,
 }
 
 require("conform").setup(options)
